ed219665f2cee3dacff42eb64b107c658cf80ecc
joao-arthur <joao.lothamer@gmail.com>
2025-07-18 19:57:37 -0300

    feat: impl Display for Rect

--- src/d2/rect/rect_u8.rs
+++ src/d2/rect/rect_u8.rs
@@ -20,6 +20,12 @@ pub fn delta_y(r: &RectU8) -> u8 {
     r.max.y - r.min.y
 }
 
+impl std::fmt::Display for RectU8 {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({}, {})", self.min.to_string(), self.max.to_string())
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use crate::d0::point::point_u8::PointU8;
@@ -32,6 +38,7 @@ mod tests {
             RectU8::of(0, 4, 64, 255),
             RectU8 { min: PointU8 { x: 0, y: 4 }, max: PointU8 { x: 64, y: 255 } }
         );
+        assert_eq!(RectU8::of(0, 4, 64, 255).to_string(), "((0, 4), (64, 255))")
     }
 
     #[test]

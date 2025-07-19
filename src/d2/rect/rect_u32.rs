3f20868e088dc18c4a5725db8173a5a0d1a2322d
joao-arthur <joao.lothamer@gmail.com>
2025-07-19 06:46:23 -0300

    feat: RectU32

---
+++ src/d2/rect/rect_u32.rs
@@ -0,0 +1,55 @@
+use crate::d0::point::point_u32::PointU32;
+
+#[derive(PartialEq, Debug, Clone)]
+pub struct RectU32 {
+    pub min: PointU32,
+    pub max: PointU32,
+}
+
+impl RectU32 {
+    pub fn of(x1: u32, y1: u32, x2: u32, y2: u32) -> Self {
+        RectU32 { min: PointU32 { x: x1, y: y1 }, max: PointU32 { x: x2, y: y2 } }
+    }
+}
+
+pub fn delta_x(r: &RectU32) -> u32 {
+    r.max.x - r.min.x
+}
+
+pub fn delta_y(r: &RectU32) -> u32 {
+    r.max.y - r.min.y
+}
+
+impl std::fmt::Display for RectU32 {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({}, {})", self.min.to_string(), self.max.to_string())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::d0::point::point_u32::PointU32;
+
+    use super::{RectU32, delta_x, delta_y};
+
+    #[test]
+    fn rect_u32() {
+        assert_eq!(
+            RectU32::of(0, 4, 64, 255),
+            RectU32 { min: PointU32 { x: 0, y: 4 }, max: PointU32 { x: 64, y: 255 } }
+        );
+        assert_eq!(RectU32::of(0, 8, 128, 1024).to_string(), "((0, 8), (128, 1024))")
+    }
+
+    #[test]
+    fn test_delta_x() {
+        assert_eq!(delta_x(&RectU32::of(0, 0, 0, 0)), 0);
+        assert_eq!(delta_x(&RectU32::of(0, 0, 4_294_967_295, 0)), 4_294_967_295);
+    }
+
+    #[test]
+    fn test_delta_y() {
+        assert_eq!(delta_y(&RectU32::of(0, 0, 0, 0)), 0);
+        assert_eq!(delta_y(&RectU32::of(0, 0, 0, 4_294_967_295)), 4_294_967_295);
+    }
+}

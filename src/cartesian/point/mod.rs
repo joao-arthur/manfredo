9f2d2cda6374b1de7d6bfe5c3423820eea22531c
joao-arthur <joao.lothamer@gmail.com>
2025-07-19 10:41:26 -0300

    feat: PointF32

--- src/cartesian/point/mod.rs
+++ src/cartesian/point/mod.rs
@@ -7,3 +7,5 @@ pub mod point_i16;
 pub mod point_i32;
 pub mod point_i64;
 pub mod point_i8;
+
+pub mod point_f32;
---
+++ src/cartesian/point/point_f32.rs
@@ -0,0 +1,31 @@
+#[derive(PartialEq, Debug, Clone)]
+pub struct PointF32 {
+    pub x: f32,
+    pub y: f32,
+}
+
+impl PointF32 {
+    fn of(x: f32, y: f32) -> Self {
+        PointF32 { x, y }
+    }
+}
+
+impl std::fmt::Display for PointF32 {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({}, {})", self.x, self.y)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::PointF32;
+
+    #[test]
+    fn point_u8() {
+        assert_eq!(PointF32::of(f32::MIN, f32::MAX), PointF32 { x: f32::MIN, y: f32::MAX });
+        assert_eq!(
+            PointF32::of(f32::MIN, f32::MAX).to_string(),
+            "(-340282350000000000000000000000000000000, 340282350000000000000000000000000000000)"
+        );
+    }
+}

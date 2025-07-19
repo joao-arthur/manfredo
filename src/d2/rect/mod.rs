751744ebc3974885a02430eacb3d122dc664efd8
joao-arthur <joao.lothamer@gmail.com>
2025-07-19 06:48:49 -0300

    feat: RectU64

--- src/d2/rect/mod.rs
+++ src/d2/rect/mod.rs
@@ -1,3 +1,4 @@
 pub mod rect_u8;
 pub mod rect_u16;
 pub mod rect_u32;
+pub mod rect_u64;
---
+++ src/d2/rect/rect_u64.rs
@@ -0,0 +1,55 @@
+use crate::d0::point::point_u64::PointU64;
+
+#[derive(PartialEq, Debug, Clone)]
+pub struct RectU64 {
+    pub min: PointU64,
+    pub max: PointU64,
+}
+
+impl RectU64 {
+    pub fn of(x1: u64, y1: u64, x2: u64, y2: u64) -> Self {
+        RectU64 { min: PointU64 { x: x1, y: y1 }, max: PointU64 { x: x2, y: y2 } }
+    }
+}
+
+pub fn delta_x(r: &RectU64) -> u64 {
+    r.max.x - r.min.x
+}
+
+pub fn delta_y(r: &RectU64) -> u64 {
+    r.max.y - r.min.y
+}
+
+impl std::fmt::Display for RectU64 {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({}, {})", self.min.to_string(), self.max.to_string())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::d0::point::point_u64::PointU64;
+
+    use super::{RectU64, delta_x, delta_y};
+
+    #[test]
+    fn rect_u64() {
+        assert_eq!(
+            RectU64::of(0, 4, 64, 255),
+            RectU64 { min: PointU64 { x: 0, y: 4 }, max: PointU64 { x: 64, y: 255 } }
+        );
+        assert_eq!(RectU64::of(0, 64, 2048, 65536).to_string(), "((0, 64), (2048, 65536))")
+    }
+
+    #[test]
+    fn test_delta_x() {
+        assert_eq!(delta_x(&RectU64::of(0, 0, 0, 0)), 0);
+        assert_eq!(delta_x(&RectU64::of(0, 0, 18_446_744_073_709_551_615, 0)), 18_446_744_073_709_551_615);
+    }
+
+    #[test]
+    fn test_delta_y() {
+        assert_eq!(delta_y(&RectU64::of(0, 0, 0, 0)), 0);
+        assert_eq!(delta_y(&RectU64::of(0, 0, 0, 18_446_744_073_709_551_615)), 18_446_744_073_709_551_615);
+    }
+}

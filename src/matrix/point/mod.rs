fe33ebb955d5798a7b6cc550240c98b3e1bb386e
joao-arthur <joao.lothamer@gmail.com>
2025-07-23 17:10:21 -0300

    feat: Matrix PointU32

--- src/matrix/point/mod.rs
+++ src/matrix/point/mod.rs
@@ -1,2 +1,3 @@
 pub mod point_u16;
+pub mod point_u32;
 pub mod point_u8;
---
+++ src/matrix/point/point_u32.rs
@@ -0,0 +1,48 @@
+#[derive(Debug, PartialEq)]
+pub struct PointU32 {
+    pub row: u32,
+    pub col: u32,
+}
+
+impl PointU32 {
+    pub fn of(row: u32, col: u32) -> Self {
+        PointU32 { row, col }
+    }
+}
+
+impl std::fmt::Display for PointU32 {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({}, {})", self.row, self.col)
+    }
+}
+
+pub fn delta_row(p1: &PointU32, p2: &PointU32) -> u32 {
+    p2.row - p1.row
+}
+
+pub fn delta_col(p1: &PointU32, p2: &PointU32) -> u32 {
+    p2.col - p1.col
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{PointU32, delta_col, delta_row};
+
+    #[test]
+    fn point_u32() {
+        assert_eq!(PointU32::of(0, u32::MAX), PointU32 { row: 0, col: u32::MAX });
+        assert_eq!(PointU32::of(0, u32::MAX).to_string(), "(0, 4294967295)");
+    }
+
+    #[test]
+    fn test_delta_row() {
+        assert_eq!(delta_row(&PointU32::of(0, 0), &PointU32::of(0, u32::MAX)), 0);
+        assert_eq!(delta_row(&PointU32::of(0, 0), &PointU32::of(u32::MAX, 0)), u32::MAX);
+    }
+
+    #[test]
+    fn test_delta_col() {
+        assert_eq!(delta_col(&PointU32::of(0, 0), &PointU32::of(u32::MAX, 0)), 0);
+        assert_eq!(delta_col(&PointU32::of(0, 0), &PointU32::of(0, u32::MAX)), u32::MAX);
+    }
+}

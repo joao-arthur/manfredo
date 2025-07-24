1135d2138716b06bd76af1f31ccea5d9837bb6a5
joao-arthur <joao.lothamer@gmail.com>
2025-07-24 11:48:34 -0300

    feat: cartesian i16 to matrix u16

--- src/transform/cartesian_to_matrix/mod.rs
+++ src/transform/cartesian_to_matrix/mod.rs
@@ -1,1 +1,2 @@
+pub mod point_i16;
 pub mod point_i8;
---
+++ src/transform/cartesian_to_matrix/point_i16.rs
@@ -0,0 +1,23 @@
+type CartesianPoint = crate::cartesian::point::point_i16::PointI16;
+type MatrixPoint = crate::matrix::point::point_u16::PointU16;
+
+pub fn cartesian_to_matrix(point: &CartesianPoint) -> MatrixPoint {
+    let temp_row = i32::from(i16::MAX) - i32::from(point.y);
+    let temp_col = i32::from(i16::MAX) + i32::from(point.x) + 1;
+    let row = temp_row.clamp(0, i32::from(u16::MAX));
+    let col = temp_col.clamp(0, i32::from(u16::MAX));
+    MatrixPoint { row: row as u16, col: col as u16 }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{CartesianPoint, MatrixPoint, cartesian_to_matrix};
+
+    #[test]
+    fn test_cartesian_to_matrix() {
+        assert_eq!(cartesian_to_matrix(&CartesianPoint::of(i16::MIN, i16::MIN)), MatrixPoint::of(u16::MAX, 0));
+        assert_eq!(cartesian_to_matrix(&CartesianPoint::of(i16::MAX, i16::MIN)), MatrixPoint::of(u16::MAX, u16::MAX));
+        assert_eq!(cartesian_to_matrix(&CartesianPoint::of(i16::MIN, i16::MAX)), MatrixPoint::of(0, 0));
+        assert_eq!(cartesian_to_matrix(&CartesianPoint::of(i16::MAX, i16::MAX)), MatrixPoint::of(0, u16::MAX));
+    }
+}

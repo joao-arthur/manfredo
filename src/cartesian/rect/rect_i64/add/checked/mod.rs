677cbdffabd3f5a6faefe8eeab8b98f7e97ccc76
joao-arthur <joao.lothamer@gmail.com>
2025-08-25 08:06:21 -0300

    wip: unify add implementation, separated tests

--- src/cartesian/rect/rect_i64/add/checked/mod.rs
+++ src/cartesian/rect/rect_i64/add/checked/mod.rs
@@ -1,9 +1,11 @@
-mod add;
-mod assign_add;
-mod try_add;
-mod try_assign_add;
+#[cfg(test)]
+mod test_checked_add_assign;
 
-pub use add::checked_add;
-pub use assign_add::checked_add_assign;
-pub use try_add::try_checked_add;
-pub use try_assign_add::try_checked_add_assign;
+#[cfg(test)]
+mod test_checked_add;
+
+#[cfg(test)]
+mod test_try_checked_add_assign;
+
+#[cfg(test)]
+mod test_try_checked_add;

3191246bf3e5860fab712859982a6837f73e1f78
João Arthur <joao.lothamer@gmail.com>
2025-09-14 10:56:46 -0300

    refactor: change structure and add 2d module

---
+++ src/transform/d2/mod.rs
@@ -0,0 +1,8 @@
+pub mod cartesian_to_matrix;
+pub mod matrix_to_cartesian;
+
+pub mod cartesian_in_cam_to_matrix;
+pub mod matrix_in_cam_to_cartesian;
+
+pub mod cartesian_to_matrix_in_cam;
+pub mod matrix_to_cartesian_in_cam;
